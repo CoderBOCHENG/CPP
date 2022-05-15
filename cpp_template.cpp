a68b77030e88f3de1a8547534fa969fde88c2102
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-05-15 17:10:01 +0800

    Create cpp_template.cpp

---
+++ cpp_template.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+#include <string.h>
+
+
+int main()
+{
+
+
+    printf("Hello C/C++!"); // f means 'formatted'
+
+
+
+
+    return 0;
+}
